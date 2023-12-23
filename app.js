38eb3df40c1c9b9ba0ea3605ae1ea35966277dac
Olga <minaeva9@gmail.com>
2023-12-23 14:10:53 -0500

    js modified

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import {
   ref,
   push,
   onValue,
-  update,
+  remove,
 } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
@@ -18,9 +18,57 @@ const endorsementData = ref(database, "user");
 
 const publishBtn = document.getElementById("publish");
 const textInput = document.getElementById("input");
+const endorsementList = document.getElementById("endorsements");
 
 // add event on button click
 publishBtn.addEventListener("click", function () {
   let userInput = textInput.value;
-  console.log(userInput);
+
+  //push data to DB
+  push(endorsementData, userInput);
+
+  clearData();
 });
+
+//fetch from DB
+onValue(endorsementData, function (snapshot) {
+  if (snapshot.exists()) {
+    let itemArr = Object.entries(snapshot.val());
+
+    clearEndorsement();
+
+    for (let i = 0; i < itemArr.length; i++) {
+      let currentItem = itemArr[i];
+      let currentItemID = currentItem[0];
+      let currentItemValue = currentItem[1];
+      addText(currentItem);
+    }
+  }
+});
+
+function clearEndorsement() {
+  endorsementList.innerHTML = "";
+}
+
+//clear text area
+function clearData() {
+  textInput.value = "";
+}
+
+//add item
+function addText(item) {
+  let itemId = item[0];
+  let itemValue = item[1];
+
+  let li = document.createElement("li");
+
+  li.textContent = itemValue;
+
+  //remove on click
+  li.addEventListener("click", function () {
+    let itemInDB = ref(database, `user/${itemId}`);
+    remove(itemInDB);
+  });
+
+  endorsementList.appendChild(li);
+}
